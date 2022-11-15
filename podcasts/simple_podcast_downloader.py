9f4bc228263848c8070e01fe76f8d7f25db81ac4
Ben Milde <bmilde@gmail.com>
2022-11-15 22:19:41 +0000

    added wget downloader

--- podcasts/simple_podcast_downloader.py
+++ podcasts/simple_podcast_downloader.py
@@ -4,15 +4,26 @@ import json
 import sys
 import yaml
 import psycopg2
+import time
+import os
+import traceback
 
 test_feed_url = "https://digitalkompakt.podigee.io/feed/mp3"
 #test_feed_url = "https://logbuch-netzpolitik.de/feed/opus"
 
+# TODO: make these configurable
 language = 'de'
-destination_folder = f'/var/www/speechcatcher.net/cache/podcasts/{language}/'
+destination_folder = f'/var/www/speechcatcher.net/cache/podcasts/{language}'
+destination_url = f'https://speechcatcher.net/cache/podcasts/{language}'
 
 p_connection = None
 p_cursor = None
+
+def ensure_dir(f):
+    d = os.path.dirname(f)
+    if not os.path.exists(d):
+        os.makedirs(d)
+
 
 def load_config(config_filename='config.yaml'):
     with open("config.yaml", "r") as stream:
@@ -108,14 +119,36 @@ def parse_and_download(feed_url):
         #print(episode_json)
         print(f"{mytype=}, {episode_title=}, {authors=}, {joined_tags}, {duration=}, {link=}, {audiolink=} {published=}")
 
+        # Only insert into DB if audio URL doesn't already exist in the DB
         if not check_audio_url(p_cursor, episode_audio_url=audiolink):
-            sql = "INSERT INTO podcasts(podcast_title, episode_title, published_date, authors, language, description, keywords, episode_url, episode_audio_url," \
-                  " cache_audio_url, cache_audio_file, transcript_file, duration, type, episode_json) VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)"
-            p_cursor.execute(sql, (podcast_title, episode_title, published, authors, language, desc, joined_tags, link, audiolink, cache_url, cache_file, transcript_file, str(duration), mytype, episode_json))
+            # Try to download audiolink and insert into db if succesful
+            try:
+                audiolink_split = audiolink.split('?')
+                assert(len(audiolink_split) == 2)
+                audio_filename = audiolink_split[0].split('/')[-1]
+                assert(len(audio_filename) > 0)
+                # insert unixtime to guarantee that the link is unique
+                retrieval_time = time.time()
+                unixtime = str(int(retrieval_time))
+                cache_file = destination_folder  + '/' + unixtime + '_' + audio_filename
+                cache_url = destination_url + '/' + unixtime + '_' + audio_filename 
+                
+                print('Downloading to:', cache_file)
+                print('Cache file will be available at:', cache_url)
+                wget.download(audiolink, out=cache_file, bar=wget.bar_thermometer)
+                print()
+
+                sql = "INSERT INTO podcasts(podcast_title, episode_title, published_date, retrieval_time, authors, language, description, keywords, episode_url, episode_audio_url," \
+                  " cache_audio_url, cache_audio_file, transcript_file, duration, type, episode_json) VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)"
+                p_cursor.execute(sql, (podcast_title, episode_title, published, str(retrieval_time), authors, language, desc, joined_tags, link, audiolink, cache_url, cache_file, transcript_file, str(duration), mytype, episode_json))
+            except:
+                print('Error occured while trying to download:', audiolink)
+                traceback.print_exc()
 
     p_connection.commit()
 
 if __name__ == "__main__":
     config = load_config()
+    ensure_dir(destination_folder)
     p_connection, p_cursor = connect_to_db(database=config["database"], user=config["user"], password=config["password"], host=config["host"], port=config["port"])
     parse_and_download(test_feed_url)
