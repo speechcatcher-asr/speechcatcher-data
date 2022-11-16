f929b178a7077b4076c12dc556f1405bd1a0a5a5
Ben Milde <bmilde@gmail.com>
2022-11-16 17:52:54 +0000

    refactored connect to db and load config

--- podcasts/simple_podcast_downloader.py
+++ podcasts/simple_podcast_downloader.py
@@ -7,6 +7,7 @@ import psycopg2
 import time
 import os
 import traceback
+from utils import load_config,  
 
 # TODO: make these configurable
 language = 'de'
@@ -26,37 +27,6 @@ def ensure_dir(f):
     d = os.path.dirname(f)
     if not os.path.exists(d):
         os.makedirs(d)
-
-
-def load_config(config_filename='config.yaml'):
-    with open("config.yaml", "r") as stream:
-        try:
-            return yaml.safe_load(stream)
-        except yaml.YAMLError as exc:
-            print(exc)
-            sys.exit(-3)
-
-def connect_to_db(database, user, password, host='127.0.0.1', port='5432'):
-    # Connect to DB
-    try:
-        mct_connection = psycopg2.connect(user = user,
-                                      password = password,
-                                      host = host,
-                                      port = port,
-                                      database = database)
-
-        mct_cursor = mct_connection.cursor()
-
-        # Print PostgreSQL version
-        mct_cursor.execute("SELECT version();")
-        record = mct_cursor.fetchone()
-        print("You are connected to Postgres - ", record,"\n")
-
-        return mct_connection, mct_cursor
-
-    except (Exception, psycopg2.Error) as error :
-        print ("Error while connecting to PostgreSQL", error)
-        sys.exit(-1)
 
 def check_audio_url(cursor, episode_audio_url):
     cursor.execute('SELECT episode_audio_url, cache_audio_url, cache_audio_file,'\
---
+++ podcasts/utils.py
@@ -0,0 +1,30 @@
+def load_config(config_filename='config.yaml'):
+    with open("config.yaml", "r") as stream:
+        try:
+            return yaml.safe_load(stream)
+        except yaml.YAMLError as exc:
+            print(exc)
+            sys.exit(-3)
+
+def connect_to_db(database, user, password, host='127.0.0.1', port='5432'):
+    # Connect to DB
+    try:
+        mct_connection = psycopg2.connect(user = user,
+                                      password = password,
+                                      host = host,
+                                      port = port,
+                                      database = database)
+
+        mct_cursor = mct_connection.cursor()
+
+        # Print PostgreSQL version
+        mct_cursor.execute("SELECT version();")
+        record = mct_cursor.fetchone()
+        print("You are connected to Postgres - ", record,"\n")
+
+        return mct_connection, mct_cursor
+
+    except (Exception, psycopg2.Error) as error :
+        print ("Error while connecting to PostgreSQL", error)
+        sys.exit(-1)
+ 
