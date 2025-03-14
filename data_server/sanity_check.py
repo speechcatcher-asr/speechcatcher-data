3c5f81fc9ad651ff119f65989b0ae090f6b12c8a
bmilde <bmilde@noreply.users.github.com>
2025-03-14 11:44:36 +0000

    improved and rewritten version of the sanity check script

--- data_server/sanity_check.py
+++ data_server/sanity_check.py
@@ -2,9 +2,10 @@ import glob
 import argparse
 from tqdm import tqdm
 from somajo import SoMaJo
-
 import subprocess
 import os
+import re
+import json
 from utils import *
 
 sql_table = 'podcasts'
@@ -13,96 +14,126 @@ def get_duration(input_video):
     cmd = ["ffprobe", "-i", input_video, "-show_entries", "format=duration", "-v", "quiet", "-sexagesimal", "-of", "csv=p=0"]
     return subprocess.check_output(cmd).decode("utf-8").strip()
 
-def check_for_degenerate_vtts(vtt_dir, audio_dir='', 
+def simple_tokenizer(text):
+    pattern = r'\b[\w-]+\b'
+    tokens = re.findall(pattern, text)
+    filtered_tokens = [token for token in tokens if re.match(r'^[\w]+(-[\w]+)*$', token)]
+    return filtered_tokens
+
+def check_for_degenerate_vtts(vtt_dir, audio_dir='', file_type='vtt', language='de',
                               possibly_corrupted_outfile='possibly_corrupted.txt',
-                              timestamps_tsv='timestamps.tsv', 
-                              p_connection=None, p_cursor=None):
-    vtts = glob.glob(f'{vtt_dir}/*.vtt')
+                              timestamps_tsv='timestamps.tsv',
+                              p_connection=None, p_cursor=None, simulate=False):
+    files = glob.glob(f'{vtt_dir}/*.{file_type}')
 
     ignore = ['-->','WEBVTT']
-
     vocab = {}
 
-    tokenizer = SoMaJo(language="de_CMC")
+    if language == 'de':
+        tokenizer = SoMaJo(language="de_CMC")
+    elif language == 'en':
+        tokenizer = SoMaJo(language="en_PTB")
+    else:
+        tokenizer = simple_tokenizer
 
-    degen_vtts = []
+    degen_files = []
 
-    for vtt in tqdm(vtts):
+    for file in tqdm(files):
         lines = {}
-        with open(vtt) as input_vtt:
-            paras = []
-            for line in input_vtt:
-                if line[-1] == '\n':
-                    line = line[:-1]
-                line_strip = line.strip()
-                if ignore[0] not in line_strip and ignore[1] not in line_strip and line_strip != '':
-                    #print(vtt, line_strip)
-                    lines[line_strip] = True
-                    paras.append(line_strip)
-                if '-->' in line:
-                    last_timestamp = line
+        paras = []
+        last_timestamp = None
 
-            if audio_dir!='':
-                vtt_filename = vtt.split('/')[-1]
-                input_audio = audio_dir + '/' + vtt_filename[:-4]
-                ffprobe_timestamp = get_duration(input_audio)
-                print(f'{vtt}', f'{last_timestamp=}', f'{ffprobe_timestamp=}')
-                with open(timestamps_tsv, 'a') as timestamps_tsv_out:
-                    timestamps_tsv_out.write(f'{vtt}\t{last_timestamp}\t{ffprobe_timestamp}\n')
+        if file_type == 'vtt':
+            with open(file) as input_vtt:
+                for line in input_vtt:
+                    if line[-1] == '\n':
+                        line = line[:-1]
+                    line_strip = line.strip()
+                    if ignore[0] not in line_strip and ignore[1] not in line_strip and line_strip != '':
+                        lines[line_strip] = True
+                        paras.append(line_strip)
+                    if '-->' in line:
+                        last_timestamp = line
+        elif file_type == 'json':
+            with open(file) as input_json:
+                data = json.load(input_json)
+                for segment in data['segments']:
+                    text = segment['text']
+                    paras.append(text)
+                    lines[text] = True
+                    last_timestamp = f"{segment['start']} --> {segment['end']}"
 
+        if audio_dir != '':
+            filename = os.path.basename(file)
+            input_audio = os.path.join(audio_dir, filename.rsplit('.', 1)[0]) + '.flac'
+            ffprobe_timestamp = get_duration(input_audio)
+            print(f'{file}', f'{last_timestamp=}', f'{ffprobe_timestamp=}')
+            with open(timestamps_tsv, 'a') as timestamps_tsv_out:
+                timestamps_tsv_out.write(f'{file}\t{last_timestamp}\t{ffprobe_timestamp}\n')
+
+        if language in ['de', 'en']:
             sentences = tokenizer.tokenize_text(paras, parallel=8)
             for sentence in sentences:
                 for token in sentence:
                     if token not in vocab:
                         vocab[token.text] = True
+        else:
+            for para in paras:
+                tokens = tokenizer(para)
+                for token in tokens:
+                    if token not in vocab:
+                        vocab[token] = True
+
         if len(lines) < 5:
             degen_num_lines = len(list(lines.keys()))
-            #print('vtt:',vtt,'len distinct lines very small:', degen_num_lines)
-            degen_vtts.append([vtt, degen_num_lines])
-    
+            degen_files.append([file, degen_num_lines])
+
     print(f"Vocabulary: {len(vocab)} words.")
 
-    # Now loop over all prolematic vtts
-    # They are moved to a sandbox dir, e.g. /corrupted/ and the DB entries are changed accordingly
-    if len(degen_vtts) > 0:
+    if len(degen_files) > 0:
         corrupted_dir = "corrupted"
-        corrupted_dir_full = str(os.path.join(os.path.dirname(degen_vtts[0][0]), corrupted_dir))
-        print('creating dir',corrupted_dir_full,'if it does not exist.')
+        corrupted_dir_full = str(os.path.join(os.path.dirname(degen_files[0][0]), corrupted_dir))
+        print('creating dir', corrupted_dir_full, 'if it does not exist.')
         ensure_dir(corrupted_dir_full)
         with open(possibly_corrupted_outfile, 'w') as outfile:
-            for vtt, degen_num_lines in degen_vtts:
-                assert(vtt is not None)
-                assert(vtt != '')
-                assert(vtt.endswith(".vtt"))
-                
-                print('vtt:',vtt,'len distinct lines very small:', degen_num_lines)
-                outfile.write(vtt + '\n')
-                
-                if f'/{corrupted_dir}/' in vtt:
-                    print('Warning, seems',vtt,'was already moved. Ignoring.')
-                    continue
+            for file, degen_num_lines in degen_files:
+                assert(file is not None)
+                assert(file != '')
+                assert(file.endswith(f".{file_type}"))
 
-                new_path = str(os.path.join(os.path.dirname(vtt), corrupted_dir, os.path.basename(vtt)))
-                assert(new_path is not None)
-                assert(new_path != '')
+                print('file:', file, 'len distinct lines very small:', degen_num_lines)
+                outfile.write(file + '\n')
 
-                if new_path != vtt:
-                    print('Move:', vtt, '->', new_path)
-                    os.rename(vtt, new_path)
-                    if p_connection is not None:
-                        print('Execute SQL:', f"UPDATE {sql_table} SET transcript_file = %s WHERE transcript_file = %s" % (new_path, vtt))
-                        p_cursor.execute(f"UPDATE {sql_table} SET transcript_file = %s WHERE transcript_file = %s", (new_path, vtt))
-                        p_connection.commit()
+                if not simulate:
+                    if f'/{corrupted_dir}/' in file:
+                        print('Warning, seems', file, 'was already moved. Ignoring.')
+                        continue
 
-    all_vtts_len = float(len(vtts))
-    degen_vtts_len = float(len(degen_vtts))
+                    new_path = str(os.path.join(os.path.dirname(file), corrupted_dir, os.path.basename(file)))
+                    assert(new_path is not None)
+                    assert(new_path != '')
 
-    degen_percent = (degen_vtts_len / all_vtts_len) * 100.
-    print(f'Possibly corrupted files: about {round(degen_percent,3)}%')
+                    if new_path != file:
+                        print('Move:', file, '->', new_path)
+                        os.rename(file, new_path)
+                        if p_connection is not None:
+                            print('Execute SQL:', f"UPDATE {sql_table} SET transcript_file = %s WHERE transcript_file = %s" % (new_path, file))
+                            p_cursor.execute(f"UPDATE {sql_table} SET transcript_file = %s WHERE transcript_file = %s", (new_path, file))
+                            p_connection.commit()
+
+    all_files_len = float(len(files))
+    degen_files_len = float(len(degen_files))
+
+    degen_percent = (degen_files_len / all_files_len) * 100.
+    print(f'Possibly corrupted files: about {round(degen_percent, 3)}%')
+    print(f'Number of files that would be changed: {len(degen_files)}')
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Check vtts for unusual repetitions and vocabulary.')
     parser.add_argument('--audio_dir', help='The directory with audio files to check (lengths)', default='', type=str)
+    parser.add_argument('--language', help='Language of the vtt files', choices=['de', 'en', 'other'], default='de', type=str)
+    parser.add_argument('--file_type', help='Type of transcript files (vtt or json)', choices=['vtt', 'json'], default='vtt', type=str)
+    parser.add_argument('--simulate', help='Simulate the operation without moving files or changing anything in the database', action='store_true')
 
     # Positional argument, without (- and --)
     parser.add_argument('vtt_dir', help='The directory with vtts to check', type=str)
@@ -114,4 +145,5 @@ if __name__ == '__main__':
 
     args = parser.parse_args()
 
-    check_for_degenerate_vtts(args.vtt_dir, args.audio_dir, p_connection=p_connection, p_cursor=p_cursor)
+    check_for_degenerate_vtts(args.vtt_dir, args.audio_dir, file_type=args.file_type, language=args.language, p_connection=p_connection, p_cursor=p_cursor, simulate=args.simulate)
+
