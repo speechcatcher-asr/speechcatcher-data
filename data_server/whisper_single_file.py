ae2010289a791740b740445526fcfaea23043b1a
bmilde <bmilde@users.noreply.github.com>
2025-03-18 22:45:48 +0000

    add initial prompt and fix copy of params

--- data_server/whisper_single_file.py
+++ data_server/whisper_single_file.py
@@ -1,6 +1,9 @@
+import os
+import copy
+
 class WhisperSingleFile:
     '''Base class for Whisper implementations that work operate on single files.'''
-    def __init__(self, model_name='large-v3', device='cuda', language='english', beam_size=5):
+    def __init__(self, model_name='large-v3', device='cuda', language='en', beam_size=5):
         self.model_name = model_name
         self.device = device
         self.language = language
@@ -22,7 +25,7 @@ class WhisperSingleFile:
     def load_model(self):
         raise NotImplementedError('This method should be overridden by subclasses.')
 
-    def transcribe(self, url, language=None, duration=-1, params=None):
+    def transcribe(self, url, language=None, duration=-1, initial_prompt=None, params=None):
         raise NotImplementedError('This method should be overridden by subclasses.')
 
     def write_vtt(self, transcript, file):
@@ -30,7 +33,7 @@ class WhisperSingleFile:
 
 class WhisperOriginal(WhisperSingleFile):
     '''A speechcatcher-data abstraction for https://github.com/openai/whisper'''
-    def __init__(self, model_name='large-v3', device='cuda', language='english', beam_size=5):
+    def __init__(self, model_name='large-v3', device='cuda', language='en', beam_size=5):
         try:
             import whisper
         except ImportError:
@@ -41,13 +44,18 @@ class WhisperOriginal(WhisperSingleFile):
     def load_model(self):
         self.model = self.whisper.load_model(self.model_name, device=self.device)
 
-    def transcribe(self, url, language=None, duration=-1, params=None):
+    def transcribe(self, url, language=None, duration=-1, initial_prompt=None, params=None):
         if params is None:
-            params = self.default_params
+            params = copy.deepcopy(self.default_params)
         if language is not None:
             params.update({'language': language})
+        if initial_prompt is not None:
+            params['initial_prompt'] = initial_prompt 
+
         print('Running single-file transcription with OG Whisper fp16 implementation on:', url)
-        print('Beam size is:', params['beam_size'])
+        print('Beam size is:', params['beam_size'] if 'beam_size' in params else 'not set')
+        print('Initial prompt is:', params['initial_prompt'] if 'initial_prompt' in params else 'N/A')
+
         return self.model.transcribe(url, **params)
 
     def write_vtt(self, result, file):
@@ -83,9 +91,9 @@ class FasterWhisper(WhisperSingleFile):
         self.model = self.WhisperModel(self.model_name, device=self.device, compute_type='float16')
         self.batched_model = self.BatchedInferencePipeline(model=self.model)
 
-    def transcribe(self, url, language=None, duration=-1, params=None):
+    def transcribe(self, url, language=None, duration=-1, initial_prompt=None, params=None):
         if params is None:
-            params = self.default_params
+            params = copy.deepcopy(self.default_params)
         if language is not None:
             params['language'] = language
         print('Running single-file transcription with CTranslate2 FasterWhisper fp16 implementation on:', url)
@@ -131,9 +139,9 @@ class WhisperX(FasterWhisper):
     def load_model(self):
         self.model = self.whisperx.load_model("large-v3", device=self.device, compute_type='float16')
 
-    def transcribe(self, url, language=None, duration=-1, params=None):
+    def transcribe(self, url, language=None, duration=-1, initial_prompt=None, params=None):
         if params is None:
-            params = self.default_params
+            params = copy.deepcopy(self.default_params)
         if language is not None:
             params['language'] = language
         print('Running single-file transcription with CTranslate2 WhisperX fp16 implementation on:', url)
@@ -174,7 +182,7 @@ class WhisperCpp(WhisperSingleFile):
     def load_model(self):
         self.model = self.Model(self.model_name, device=self.device)
 
-    def transcribe(self, url, language=None, duration=-1, params=None):
+    def transcribe(self, url, language=None, duration=-1, initial_prompt=None, params=None):
         if params is None:
             params = self.default_params
         if language is not None:
