1f120ec3770b6fcbb190dec61acd973102e6ce8b
bmilde <bmilde@noreply.users.github.com>
2025-04-04 01:10:00 +0000

    added lang detect based on the description of a podcast episode

--- podcasts/simple_podcast_downloader.py
+++ podcasts/simple_podcast_downloader.py
@@ -9,26 +9,27 @@ import os
 import traceback
 import argparse
 import subprocess
-from utils import load_config, connect_to_db 
+from langdetect import detect, LangDetectException
+from utils import load_config, connect_to_db
 
 p_connection = None
 p_cursor = None
 
-# loads a new line seperated text file with a list of feeds
+# Loads a new line-separated text file with a list of feeds
 def load_feeds(list_file):
     with open(list_file) as list_file_in:
         return [elem.rstrip('\n') for elem in list_file_in]
 
-# creates dir f if it doesnt exist
+# Creates dir f if it doesn't exist
 def ensure_dir(f):
     d = os.path.dirname(f)
     if not os.path.exists(d):
         os.makedirs(d)
 
-# checks if an audio url is already in the db
+# Checks if an audio URL is already in the DB
 def check_audio_url(cursor, episode_audio_url):
-    cursor.execute('SELECT episode_audio_url, cache_audio_url, cache_audio_file,'\
-                   'transcript_file from podcasts where episode_audio_url=%s', (episode_audio_url,) )
+    cursor.execute('SELECT episode_audio_url, cache_audio_url, cache_audio_file,'
+                   'transcript_file from podcasts where episode_audio_url=%s', (episode_audio_url,))
     record = cursor.fetchone()
     if record is not None and len(record) > 0:
         episode_audio_url, cache_audio_url, cache_audio_file, transcript_file = record
@@ -36,8 +37,7 @@ def check_audio_url(cursor, episode_audio_url):
         return True
     return False
 
-# Some audio links include a weird sort of tracking link that this function removes
-# 'https://chtbl.com/track/E43E46/https://lcdn.letscast.fm/media/podcast/62368df4/episode/3b2c0786.mp3?t=1618380826' 
+# Removes tracking links from audio links
 def remove_tracking_link(audiolink):
     if audiolink.count('https://') >= 2:
         audiolink = 'https://' + audiolink.split('https://')[-1]
@@ -45,15 +45,14 @@ def remove_tracking_link(audiolink):
         audiolink = 'http://' + audiolink.split('http://')[-1]
     return audiolink
 
-# parse feed feed_url and download all episodes
-def parse_and_download(feed_url):
+# Parses feed feed_url and downloads all episodes
+def parse_and_download(feed_url, filter_language):
     d = feedparser.parse(feed_url)
 
-    # If we cant find at least a podcast title, something went wrong or feed_url isnt a RSS feed
-    # in this case we just print a warning and skip downloading anything
+    # If we can't find at least a podcast title, something went wrong or feed_url isn't a RSS feed
     if not 'title' in d.feed:
         print(d.feed)
-        print(f'Skipping {feed_url=}, no title found. Maybe not a rss feed?')
+        print(f'Skipping {feed_url=}, no title found. Maybe not a RSS feed?')
         return
 
     podcast_title = d.feed['title']
@@ -63,10 +62,9 @@ def parse_and_download(feed_url):
     if 'author' in d.feed:
         feed_authors = d.feed['author']
 
-    # gather meta data for each episode and start downloading
+    # Gather meta data for each episode and start downloading
     for episode in d.entries:
-
-        episode_title = episode["title"] if "title" in episode else ''   
+        episode_title = episode["title"] if "title" in episode else ''
         desc = episode["description"] if "description" in episode else ''
         published = episode["published"] if "published" in episode else ''
         tags = []
@@ -75,7 +73,17 @@ def parse_and_download(feed_url):
         audiolink = ''
         mytype = ''
 
-        # find the audio link in the links section
+        # Detect language of the description
+        try:
+            detected_language = detect(desc)
+        except LangDetectException:
+            detected_language = None
+
+        if filter_language and detected_language != filter_language:
+            print(f'Skipping episode "{episode_title}" because its description is not in the specified language ({filter_language}). Detected language: {detected_language}')
+            continue
+
+        # Find the audio link in the links section
         for elem in episode["links"]:
             if elem["type"].startswith("audio"):
                 mytype = elem["type"]
@@ -86,16 +94,16 @@ def parse_and_download(feed_url):
                     continue
                 link = elem["href"]
 
-        # remove tracking link that makes downloading impossible
+        # Remove tracking link that makes downloading impossible
         audiolink = remove_tracking_link(audiolink)
 
-        # add tags (keywords) to list if available
-        if 'tags' in episode: 
+        # Add tags (keywords) to list if available
+        if 'tags' in episode:
             for tag in episode['tags']:
                 tags.append(tag['term'])
 
-        # get the duration, sometimes its in seconds sometimes hh:mm:ss.
-        # we convert everything to seconds
+        # Get the duration, sometimes it's in seconds, sometimes hh:mm:ss.
+        # We convert everything to seconds
         if 'itunes_duration' in episode:
             duration = episode['itunes_duration']
 
@@ -121,10 +129,10 @@ def parse_and_download(feed_url):
                     continue
         else:
             print('Warning: no itunes_duration in episode')
-        print(episode) 
+        print(episode)
 
-        # some feeds have episode authors, some don't
-        # if available take them, if not, use the overall feed author information
+        # Some feeds have episode authors, some don't
+        # If available take them, if not, use the overall feed author information
         if "authors" in episode and "name" in episode["authors"]:
             authors = ' '.join(author["name"] for author in episode["authors"])
         else:
@@ -137,34 +145,34 @@ def parse_and_download(feed_url):
         model_name = ''
 
         # Use default=str for delta timedelta objects (parsed dates), since the json.dumps function can't handle them otherwise
-        episode_json = json.dumps(episode, default=str) 
+        episode_json = json.dumps(episode, default=str)
 
         #print(episode_json)
         print(f"{mytype=}, {episode_title=}, {authors=}, {joined_tags}, {duration=}, {link=}, {audiolink=} {published=}")
 
         # Only insert into DB if audio URL doesn't already exist in the DB
         if not check_audio_url(p_cursor, episode_audio_url=audiolink):
-            # Try to download audiolink and insert into db if succesful
+            # Try to download audiolink and insert into DB if successful
             try:
                 audiolink_split = audiolink.split('?')
                 assert(len(audiolink_split) <= 2)
-                
+
                 audio_filename = audiolink_split[0].split('/')[-1].replace('%','_')
                 assert(len(audio_filename) > 0)
 
-                # insert unixtime to guarantee that the link is unique
+                # Insert unixtime to guarantee that the link is unique
                 retrieval_time = time.time()
                 unixtime = str(int(retrieval_time))
                 cache_file = destination_folder  + '/' + unixtime + '_' + audio_filename
-                cache_url = destination_url + '/' + unixtime + '_' + audio_filename 
-        
+                cache_url = destination_url + '/' + unixtime + '_' + audio_filename
+
                 assert(os.path.exists(cache_file) == False)
 
                 print('Downloading to:', cache_file)
                 print('Cache file will be available at:', cache_url)
 
                 subprocess.run([
-                    "wget", "--no-check-certificate", "--max-redirect=15", 
+                    "wget", "--no-check-certificate", "--max-redirect=15",
                     "--retry-connrefused", "--tries=5", "-O", cache_file, audiolink
                 ], check=True)
 
@@ -172,47 +180,71 @@ def parse_and_download(feed_url):
                     file_size_mb = os.path.getsize(cache_file) / (1024 * 1024)
                     print(f'Downloaded file: {cache_file} ({file_size_mb:.2f} MB)')
 
-                    sql = "INSERT INTO podcasts(podcast_title, episode_title, published_date, retrieval_time, authors, language, description, keywords, episode_url, episode_audio_url," \
-                      " cache_audio_url, cache_audio_file, transcript_file, duration, type, episode_json, model) VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)"
-                    p_cursor.execute(sql, (podcast_title, episode_title, published, str(retrieval_time), authors, language, desc, joined_tags, link, audiolink, cache_url, cache_file, transcript_file, str(duration), mytype, episode_json, model_name))
+                    # Prepare the SQL query with placeholders
+                    sql = """
+                    INSERT INTO podcasts(
+                        podcast_title, episode_title, published_date, retrieval_time, authors, language,
+                        description, keywords, episode_url, episode_audio_url, cache_audio_url,
+                        cache_audio_file, transcript_file, duration, type, episode_json, model
+                    ) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)
+                    """
+
+                    # Values to be inserted
+                    values = (
+                        podcast_title, episode_title, published, str(retrieval_time), authors, language,
+                        desc, joined_tags, link, audiolink, cache_url, cache_file, transcript_file,
+                        str(duration), mytype, episode_json, model_name
+                    )
+
+                    # Print the SQL query with the actual values
+                    # print("Executing SQL query:")
+                    # print(sql % values)
+
+                    # Execute the SQL query
+                    p_cursor.execute(sql, values)
                     p_connection.commit()
 
                     print("SUCCESS")
                 else:
                     print('Error: Downloaded file is empty or does not exist:', cache_file)
             except:
-                print('Error occured while trying to download:', audiolink)
+                print('Error occurred while trying to download:', audiolink)
                 traceback.print_exc()
 
     p_connection.commit()
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser("This is a small utility to download a list of podcast episodes and puts metadata into a postgres database.")
+    parser = argparse.ArgumentParser("This is a small utility to download a list of podcast episodes and puts metadata into a PostgreSQL database.")
     parser.add_argument("rss_feed_list", help="List of RSS feeds to process (one per line)")
+    parser.add_argument("--language", default='en', help="Language of the podcasts to download (default: 'en')")
+    parser.add_argument("--filter-language", default=None, help="Filter podcasts by description language using langdetect (e.g., 'en' for English)")
     args = parser.parse_args()
 
     rss_feed_list = args.rss_feed_list
+    language = args.language
+    filter_language = args.filter_language
 
     config = load_config()
-    
+
     podcast_language = config["podcast_language"]
     language = podcast_language
 
-    destination_folder = config["download_destination_folder"].replace('{podcast_language}',podcast_language)
-    destination_url = config["download_destination_url"].replace('{podcast_language}',podcast_language)
+    destination_folder = config["download_destination_folder"].replace('{podcast_language}', podcast_language)
+    destination_url = config["download_destination_url"].replace('{podcast_language}', podcast_language)
 
     ensure_dir(destination_folder)
     p_connection, p_cursor = connect_to_db(database=config["database"], user=config["user"],
             password=config["password"], host=config["host"], port=config["port"])
-   
+
     rss_feeds_in_list = load_feeds(rss_feed_list)
 
     for feed_url in rss_feeds_in_list:
         print('Downloading from:', feed_url)
         time.sleep(1)
         try:
-            parse_and_download(feed_url)
+            parse_and_download(feed_url, filter_language)
         except:
-            print('Global and unexpected error occured while trying to process feed:', feed_url)
+            print('Global and unexpected error occurred while trying to process feed:', feed_url)
             traceback.print_exc()
             print('Warning: will ignore entire feed!')
+
